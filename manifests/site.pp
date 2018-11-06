ee27713d6bf49958092d14191f80077e4e4dedcc
Sai Yin Tong <34279947+saiyintong@users.noreply.github.com>
2018-11-06 11:06:02 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,9 +7,7 @@ node 'master.puppet.vm' {
     content => "Welcome to ${fqdn}\n",
   }
 }
-node 'minetest.puppet.vm' {
-  include role::minecraft_server
-}
+
 node /^web/ {
   include role::app_server
 }
